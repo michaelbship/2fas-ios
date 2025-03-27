cf44314004668e49f4dc80aea1d36d0d6dad4e13
Zbigniew Cisiński <zbigniew@cisinski.com>
2025-03-27 21:59:47 +0100

    #1653 Migration UI fixes

--- TwoFAS/Data/MainRepository/MainRepositoryImpl.swift
+++ TwoFAS/Data/MainRepository/MainRepositoryImpl.swift
@@ -54,6 +54,7 @@ final class MainRepositoryImpl: MainRepository {
     let iconDescriptionDatabase: IconDescriptionDatabase = IconDescriptionDatabaseImpl()
     let initialPermissionStateDataController = PermissionsStateDataController()
     let mdmRepository: MDMRepository = MDMRepositoryImpl()
+    let syncMigration: SyncMigrationHandling
     
     let serviceNameTranslation: String
     let notificationCenter = NotificationCenter.default
@@ -137,7 +138,8 @@ final class MainRepositoryImpl: MainRepository {
             timeVerificationController: TimeVerificationController(),
             cloudHandler: SyncInstance.getCloudHandler(),
             logDataChange: SyncInstance.logDataChange,
-            serviceNameTranslation: serviceNameTranslation
+            serviceNameTranslation: serviceNameTranslation,
+            syncMigration: SyncInstance.getSyncMigrationHandler()
         )
     }
     
@@ -152,7 +154,8 @@ final class MainRepositoryImpl: MainRepository {
         timeVerificationController: TimeVerificationController,
         cloudHandler: CloudHandlerType,
         logDataChange: LogDataChange,
-        serviceNameTranslation: String
+        serviceNameTranslation: String,
+        syncMigration: SyncMigrationHandling
     ) {
         self.service = storage.service
         self.pushNotifications = pushNotifications
@@ -170,6 +173,7 @@ final class MainRepositoryImpl: MainRepository {
         self.userDefaultsRepository = UserDefaultsRepositoryImpl()
         self.logDataChange = logDataChange
         self.serviceNameTranslation = serviceNameTranslation
+        self.syncMigration = syncMigration
         
         storageRepository = storage.storageRepository
         timeVerificationController.log = { value in Log(value, module: .counter) }
